09c1fde46e48ab610756357293606ec1ff02e7ce
Rhodri Thomas <rhoddithomas@outlook.com>
2023-11-02 18:23:19 +0000

    Basic framework for extension

--- background.js
+++ background.js
@@ -4,6 +4,10 @@ chrome.contextMenus.create({
     contexts: ["link"] //Only allows context menu for links
 })
 
-chrome.contextMenus.onClicked.addListener((info, tab, linkUrl) =>{
+chrome.contextMenus.onClicked.addListener((info) =>{
     console.log(info) //Information about the item clicked and the context where the click happened
+    
+    //Extracts URL link as variable and displays it in the log
+    const linkUrl = info.linkUrl; 
+    console.log(linkUrl)
 })
