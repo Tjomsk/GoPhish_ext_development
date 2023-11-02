aed4375f48ea7a9c42d2827aa95dc761819c0383
Rhodri Thomas <rhoddithomas@outlook.com>
2023-11-02 17:29:57 +0000

    Update

--- background.js
+++ background.js
@@ -4,8 +4,6 @@ chrome.contextMenus.create({
     contexts: ["link"] //Only allows context menu for links
 })
 
-chrome.contextMenus.onClicked.addListener((info, tab) =>{
-    console.log(info)
-    console.log(tab)
-    console.log("TEST1")
+chrome.contextMenus.onClicked.addListener((info, tab, linkUrl) =>{
+    console.log(info) //Information about the item clicked and the context where the click happened
 })
