3c405f7c4cc4fe888b00b537e11e3f4f0838eece
Rhodri Thomas <rhoddithomas@outlook.com>
2023-12-07 14:54:46 +0000

    edits_07/12/2023

--- popup.js
+++ popup.js
@@ -1,1 +1,15 @@
 //Local js file for the extensions DOM
+
+const urlList = document.getElementById('url-list');
+
+chrome.aciton.onClicked.addListener(()=>{
+    chrome.runtime.sendMessage({aciton: 'collectUrls' }, (response) => {
+        if(response.urls){
+            response.urls.array.forEach(url => {
+                const li = document.createElement('li');
+                li.textContent = url;
+                urlList.appendChild(li);
+            });
+        }
+    })
+})
