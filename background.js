4e6bb68e860e13150369c4042a5e6e160894864a
Tjomsk <rhoddithomas@outlook.com>
2024-01-11 12:22:15 +0100

    Making updates

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'url') {
       const selectedUrl = message.url;
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.create({ url: 'popup.html', active: true, tabId: tabs[0].id });
+        chrome.tabs.create({ url: 'GoPhish.html', active: true, tabId: tabs[0].id });
       });
     }
   });
