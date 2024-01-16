58e50dcb52212eeb70cd5a2ddf659a7553295f8e
Tjomsk <rhoddithomas@outlook.com>
2024-01-16 18:42:03 +0100

    Edits 16_01_2024

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.onClicked.addListener((info) => {
+    if (info.menuItemId === 'send-url') {
+      const selectedUrl = info.linkUrl;
+      chrome.runtime.sendMessage({ url: selectedUrl });
+    }
+  });
+});
+
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'url') {
       const selectedUrl = message.url;
